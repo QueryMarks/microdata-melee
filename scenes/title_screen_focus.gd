1078c4259d00336c3e9632a9b64bb6f33bac63ae
Ethelyn <metallicminiboss@gmail.com>
2023-02-10 16:22:34 -0500

    Game can end now if you choose to be "NOT GAMING"
    Signed-off-by: Ethelyn <metallicminiboss@gmail.com>

--- scenes/title_screen_focus.gd
+++ scenes/title_screen_focus.gd
@@ -5,7 +5,10 @@ extends VBoxContainer
 func _ready():
 	$Button1.grab_focus()
 	$Button1.pressed.connect(css_pressed)
+	$Button2.pressed.connect(quit_game)
 	
 func css_pressed():
 	get_tree().change_scene_to_file("res://scenes/character_select_screen.tscn")
 
+func quit_game():
+	get_tree().quit()
