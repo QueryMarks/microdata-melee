e30fd2a96ae7012d0c1a0e8331a4b51a4b8e5f1c
Ethelyn <metallicminiboss@gmail.com>
2023-03-03 15:31:12 -0500

    Added a functional backdash state! I also updated the input for dash and backdash.
    Signed-off-by: Ethelyn <metallicminiboss@gmail.com>

--- scripts/states/general/backdash_state.gd
+++ scripts/states/general/backdash_state.gd
@@ -4,9 +4,17 @@ class_name BackdashState
 
 # Called when the node enters the scene tree for the first time.
 func enter():
-	player.velocity.x = -500*player.my_facing
+	player.anim_play(player.character_name+"/backdash")
+	player.velocity.x = -200*player.my_facing
+	player.velocity.y = -50
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	if player.is_on_floor():
+		state_machine.change_state(player.move_list.my_idle.new())
+	else:
+		player.velocity.y += 5
+		if abs(player.velocity.x) > 50:
+			player.velocity.x -= 15*sign(player.velocity.x)
+	
