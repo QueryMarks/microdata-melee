b66ff68dfb952bc8901a271244ee92ce84b50dd2
Ethelyn <metallicminiboss@gmail.com>
2023-06-04 13:34:49 -0400

    Further work on summons.

--- card_summon_a.gd
+++ card_summon_a.gd
@@ -1,13 +1,16 @@
 extends GroundSpecialState
 class_name CardSummonaState
 
-
+var summon_anims = ["card_summon", "card_summon_2", "card_summon_3", "card_summon_4"]
 # Called when the node enters the scene tree for the first time.
 func enter():
 	super()
 	var deck = player.get_node("Deck")
+	var rando = randi_range(0, summon_anims.size()-1)
+	player.anim_play(player.character_name+"/"+summon_anims[rando])
+	
 	if (deck.hand[0]):
-		player.anim_play(player.character_name+"/card_summon")
+		
 		var my_summon = deck.hand[0].rank.summon.instantiate()
 		var my_summon_script = my_summon.get_child(0)
 		my_summon_script.player = player
