7253edda01adc0704178ecd367bb135cca3db70e
Ethelyn <metallicminiboss@gmail.com>
2023-09-26 12:48:59 -0400

    Updated character_list_generator logic to only generate a character list if the list is empty. This means we don't get unfinished characters on the select screen if we just define what we want visible. Minor changes to title screen text and removed Street Fighter logo.
    Signed-off-by: Ethelyn <metallicminiboss@gmail.com>

--- character_list_generator.gd
+++ character_list_generator.gd
@@ -1,18 +1,21 @@
 extends Node
 var character_path = "res://resources/characters"
-var character_list = []
-var p1_palettes = [""]
-var p2_palettes = [""]
+var character_list = ["card", "lamb"]
+var p1_palettes = []
+var p2_palettes = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	character_list_make()
+	if character_list == []:
+		character_list_make()
 	
 	#weird bug with the window being sized incorrectly -- maybe due to godot 4 weirdness? anyway this line is here so the window will be properly sized to the settings
 	DisplayServer.window_set_size(Vector2i(ProjectSettings.get_setting("display/window/size/viewport_width"),ProjectSettings.get_setting("display/window/size/viewport_height")))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func character_list_make():
+	
 	var dir = DirAccess.open(character_path)
+	
 	if dir:
 		dir.list_dir_begin()
 		var file_name = dir.get_next()
@@ -21,15 +24,17 @@ func character_list_make():
 				#if dir.file_exists(character_path+"/"+file_name+"/"+file_name+".tscn") or dir.file_exists(character_path+"/"+file_name+"/"+file_name+".tscn.import"):
 					character_list.append(file_name)
 			file_name = dir.get_next()
+		print(character_list)
+		
 	else:
 		print("uh oh! problem")
 		
 func palette_list_make(player_index : int, char_name : String):
 	print("MAKIN A LIST CHECKIN IT LIST")
 	if player_index == 1:
-		p1_palettes = [""]
+		p1_palettes = []
 	elif player_index == 2:
-		p2_palettes = [""]
+		p2_palettes = []
 	var dir = DirAccess.open(character_path+"/"+char_name+"/palettes")
 	if dir:
 		dir.list_dir_begin()
