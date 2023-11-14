3c2bdff16fb2b5ff7f35a696dff6a0e5203eb37d
Ethelyn <metallicminiboss@gmail.com>
2023-11-14 15:25:52 -0500

    Allowed dash to transition to jump (and keep speed).
    Signed-off-by: Ethelyn <metallicminiboss@gmail.com>

--- scripts/states/general/dash_state.gd
+++ scripts/states/general/dash_state.gd
@@ -8,6 +8,9 @@ func enter():
 	player.velocity.x = 150 * player.my_facing	
 	
 func _physics_process(_delta):
+	if (Input.is_action_pressed(player.input_up)):
+		state_machine.change_state(JumpState.new())
+	#INTENTIONALLY NOT AN ELIF!! We want the following statement to override the player's velocity with dash velocity *even if* we just set it in JumpState.enter(). This makes dash jumps faster! We might rework this when prejump is added.
 	if ((Input.is_action_pressed(player.input_left) and !Input.is_action_pressed(player.input_right) and player.my_facing == -1)) or ((Input.is_action_pressed(player.input_right) and !Input.is_action_pressed(player.input_left) and player.my_facing == 1)) or (Input.is_action_pressed(player.input_o)) or dash_timer < 15:
 		dash_timer += 1
 		player.velocity.x = 150 * player.my_facing	
