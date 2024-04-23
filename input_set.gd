d30d97020848e00ad0b35ff8b6396e47ebba4ae4
Ethelyn C Graham <ethelyn.graham@owl.ucc.edu>
2024-04-23 15:20:53 -0400

    Started work on input remapping. Added BGM node to main scene.
    Signed-off-by: Ethelyn <metallicminiboss@gmail.com>

---
+++ input_set.gd
@@ -0,0 +1,10 @@
+class_name InputSet
+
+
+var a : InputEvent
+var b : InputEvent
+var c : InputEvent
+var up : InputEvent
+var down : InputEvent
+var left : InputEvent
+var right : InputEvent
---
+++ remap_focus.gd
@@ -0,0 +1,9 @@
+extends VBoxContainer
+
+
+var input_set : InputSet
+func _input(event):
+	print(event.as_text())
+# Called when the node enters the scene tree for the first time.
+func get_inputs(player_index : int):
+	pass
