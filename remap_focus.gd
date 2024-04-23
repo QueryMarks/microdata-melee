0e397456082adb6f3e2a3086b13a0b39c91a4a91
Ethelyn C Graham <ethelyn.graham@owl.ucc.edu>
2024-04-23 15:25:05 -0400

    Input remapping work.
    Signed-off-by: Ethelyn C Graham <ethelyn.graham@owl.ucc.edu>

--- remap_focus.gd
+++ remap_focus.gd
@@ -2,8 +2,10 @@ extends VBoxContainer
 
 
 var input_set : InputSet
+var accepting_inputs = false
 func _input(event):
-	print(event.as_text())
+	if accepting_inputs:
+		print(event.as_text())
+
+
 # Called when the node enters the scene tree for the first time.
-func get_inputs(player_index : int):
-	pass
